2a570e7ad473352b2cda53b03c2c1baab1b69551
Jack_Pronin <pronin.jack@gmail.com>
2025-09-15 20:00:25 +0300

    slider

---
+++ scripts/main.js
@@ -0,0 +1,12 @@
+$(document).ready(function () {
+    $('.day-product__slider').slick({
+        arrows: true,
+        vertical: true,
+        verticalSwiping: true,
+        slidesToShow: 3,
+        slidesToScroll: 1,
+        infinite: true,
+        centerMode: true,
+        centerPadding: 0,
+    });
+})
