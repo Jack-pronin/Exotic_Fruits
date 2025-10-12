382736e424433d73ed6ec3ceeb2ad99a43220ae1
Jack_Pronin <pronin.jack@gmail.com>
2025-10-12 23:40:16 +0300

    709px adaptive

--- scripts/main.js
+++ scripts/main.js
@@ -6,14 +6,33 @@ $(document).ready(function () {
         slidesToShow: 3,
         slidesToScroll: 1,
         infinite: true,
-        centerMode: true,
         centerPadding: 0,
+        responsive: [
+            {
+                breakpoint: 1120,
+                settings: {
+                    vertical: false,
+                    verticalSwiping: false,
+                    centerMode: false,
+                    slidesToShow: 3,
+                    slidesToScroll: 1
+                }
+            }
+        ]
     });
 
     $('.reviews__slider').slick({
         slidesToShow: 2,
         slidesToScroll: 1,
         infinite: true,
+        responsive: [
+            {
+                breakpoint: 1079,
+                settings: {
+                    slidesToShow: 1,
+                }
+            }
+        ]
     })
 
     $( function() {
