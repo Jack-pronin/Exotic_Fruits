75d372c3c3cd17744dcf72f93f1ba2dd5b4bb01e
Jack <pronin.jack@gmail.com>
2025-10-18 04:26:06 +0300

    product tab

--- scripts/main.js
+++ scripts/main.js
@@ -7,35 +7,62 @@ $(document).ready(function () {
         slidesToScroll: 1,
         infinite: true,
         centerPadding: 0,
-        responsive: [
-            {
-                breakpoint: 1120,
-                settings: {
-                    vertical: false,
-                    verticalSwiping: false,
-                    centerMode: false,
-                    slidesToShow: 3,
-                    slidesToScroll: 1
-                }
+        responsive: [{
+            breakpoint: 1120, settings: {
+                vertical: false, verticalSwiping: false, centerMode: false, slidesToShow: 3, slidesToScroll: 1
             }
-        ]
+        }]
     });
 
     $('.reviews__slider').slick({
-        slidesToShow: 2,
-        slidesToScroll: 1,
-        infinite: true,
-        responsive: [
-            {
-                breakpoint: 1079,
-                settings: {
-                    slidesToShow: 1,
-                }
+        slidesToShow: 2, slidesToScroll: 1, infinite: true, responsive: [{
+            breakpoint: 1079, settings: {
+                slidesToShow: 1,
             }
-        ]
-    })
+        }]
+    });
 
-    $( function() {
-        $( "#accordion" ).accordion();
-    } );
+    $("#accordion").accordion();
+
+
+
+    const tab = document.querySelector('.products__tab');
+    const buttons = tab.querySelectorAll('.products__tab-choice-btn');
+    let activeBtn = tab.querySelector('.active');
+
+    function moveHighlight(btn) {
+        const rect = btn.getBoundingClientRect();
+        const tabRect = tab.getBoundingClientRect();
+
+        const width = rect.width;
+        tab.style.setProperty('--hl-width', `${width}px`);
+        const left = rect.left - tabRect.left - 1.6;
+        tab.style.setProperty('--hl-left', `${left}px`);
+    }
+
+    moveHighlight(activeBtn);
+
+    buttons.forEach((btn) => {
+        btn.addEventListener('mouseenter', () => {
+            moveHighlight(btn);
+            if (btn !== activeBtn) {
+                activeBtn.classList.remove('active');
+            }
+        });
+
+        btn.addEventListener('mouseleave', () => {
+            moveHighlight(activeBtn);
+            activeBtn.classList.add('active');
+        });
+
+        btn.addEventListener('click', () => {
+            if (activeBtn !== btn) {
+                activeBtn.classList.remove('active');
+                btn.classList.add('active');
+                activeBtn = btn;
+                moveHighlight(activeBtn);
+            }
+        });
+    });
+
 })
